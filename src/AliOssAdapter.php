70321a4ebfc0dd188b9051686e764892e95742ed
panda <839027631@qq.com>
2020-03-20 14:53:30 +0800

    修改url方法

--- src/AliOssAdapter.php
+++ src/AliOssAdapter.php
@@ -567,8 +567,10 @@ class AliOssAdapter extends AbstractAdapter
      */
     public function getUrl( $path )
     {
-        if (!$this->has($path)) throw new FileNotFoundException($filePath.' not found');
-        return ( $this->ssl ? 'https://' : 'http://' ) . ( $this->isCname ? ( $this->cdnDomain == '' ? $this->endPoint : $this->cdnDomain ) : $this->bucket . '.' . $this->endPoint ) . '/' . ltrim($path, '/');
+        if (!$this->has($path)) {
+            throw new FileNotFoundException($path.' not found');
+        }
+        return ( $this->ssl ? 'https://' : 'http://' ) . ( $this->isCname ? ( $this->cdnDomain == '' ? $this->endPoint : $this->cdnDomain ) : $this->bucket . '.' . $this->endPoint ) . '/' . $this->applyPathPrefix($path);
     }
 
     /**
